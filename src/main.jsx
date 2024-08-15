e4f607192bdc2fe099ee1175b38513d857dcc75d
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2024-08-15 12:04:51 +0600

    first commit for setup fies

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className='font-bold text-red-900'>Hello world!</div>,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
