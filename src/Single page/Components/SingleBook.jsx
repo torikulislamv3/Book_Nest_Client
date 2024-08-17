0464e249f0d1460b2270616b5bb5019957c36420
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2024-08-17 11:27:30 +0600

    all product map

---
+++ src/Single page/Components/SingleBook.jsx
@@ -0,0 +1,10 @@
+const SingleBook = ({item}) => {
+    // const product = {_id, name, image, description, price, category, ratings, createdDate, createdTime} = item;
+  return (
+    <div>
+        this is book card
+    </div>
+  )
+}
+
+export default SingleBook
--- src/Single page/Home.jsx
+++ src/Single page/Home.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom"
+import SingleBook from "./Components/SingleBook";
 
 const Home = () => {
   const Book_items = useLoaderData();
@@ -6,7 +7,9 @@ const Home = () => {
   return (
     <div>
         this is loader for {Book_items.length}
-        
+        {
+          Book_items.map((item)=> <SingleBook key={item._id} item = {item}></SingleBook>)
+        }
     </div>
   )
 }
