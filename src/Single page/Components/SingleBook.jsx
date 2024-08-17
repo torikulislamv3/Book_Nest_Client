8e051cf22cdee1f307939a8920f4e887102158c3
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2024-08-17 12:05:38 +0600

    ui ready for category search box filtering

--- src/Single page/Components/SingleBook.jsx
+++ src/Single page/Components/SingleBook.jsx
@@ -1,7 +1,7 @@
 const SingleBook = ({item}) => {
     const {_id, name, image, description, price, category, ratings, createdDate, createdTime} = item;
   return (
-    <div className="card bg-white w-96 shadow-xl h-auto">
+    <div className="card bg-blak w-96 shadow-xl h-auto">
     <figure className="px-10 pt-10">
       <img
         src={image}
@@ -9,19 +9,19 @@ const SingleBook = ({item}) => {
         className="rounded-xl" />
     </figure>
     <div className="card-body items-center text-center">
-      <h2 className="card-title">{name}</h2>
+      <h2 className="card-title text-yellow-300">{name}</h2>
       <div className="grid grid-cols-2">
             <div>
                <p>
                {description}
                </p>
-               <p>
-                {category}
+               <p className="text-red-500">
+                 {category}
                </p>
             </div>
             <div>
-                <p>{price}</p>
-                <p>{ratings}</p>
+                <p>${price}</p>
+                <p> ratings  : {ratings}</p>
                 <p>{createdDate}</p>
                 <p>{createdTime}</p>
             </div>
--- src/Single page/Home.jsx
+++ src/Single page/Home.jsx
@@ -6,6 +6,28 @@ const Home = () => {
   console.log(Book_items)
   return (
     <div>
+
+      <section className="flex justify-between mt-6">
+      <select className="select select-bordered w-full max-w-xs">
+  <option disabled selected>Select Category</option>
+  <option>Programming Language</option>
+  <option>Science Fiction</option>
+  <option>Motivational</option>
+  <option>Self Development</option>
+</select>
+
+<input
+  type="text"
+  placeholder="Search Your Book"
+  className="input input-bordered input-primary w-full max-w-xs" />
+
+<select className="select select-bordered w-full max-w-xs">
+  <option disabled selected>Select Option</option>
+  <option> Low to High</option>
+  <option>High to Low</option>
+  <option>Newest first</option>
+</select>
+      </section>
        
        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
        {
